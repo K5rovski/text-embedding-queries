88a5c63050f3376902138ac8a7979e468fea3174
K5rovski <K5rovski@gmail.com>
2022-06-19 21:56:33 +0200

    updated config to latest nb version
    Former-commit-id: 90a0a3c512b92a74ee790f9549bd464565f64a7d

--- src/config.py
+++ src/config.py
@@ -39,61 +39,81 @@ AmazonS3/userguide
 amazonglacier/dev
 lambda/dg
 amazon-mq/developer-guide
-amazon-mq/migration-guide'''.splitlines()
+amazon-mq/migration-guide
+AWSEC2/UserGuide
+AmazonRDS/UserGuide
+AmazonRDS/AuroraUserGuide
+secretsmanager/userguide
+cloudhsm/userguide
+IAM/UserGuide'''.splitlines()
 
+url_checks = dict(
+    check_ebs=lambda url: True if (('ebs' in url.lower() and 'AWSEC2' in url) or 'AWSEC2' not in url) else False
+)
 
-serviceD=dict(streams='streams',
-              firehose='firehose',
-              kinesisanalytics='analytics',
-              athena='athena',
-              redshift='redshift',
-              quicksight='quicksight',
-              glue='glue',
-              emr='emr',
-              dms='dms',
-              kms='kms',
-              AmazonS3='s3',
-              datapipeline='datapipeline',
-              elasticache='elasticache',
-              directconnect='directconnect',
-              sagemaker='sagemaker',
-              sns='sns',
-              snowball='snowball',
-              amazondynamodb='dynamodb',
-              AWSSimpleQueueService='sqs',
-              AmazonElastiCache='elasticache',
-              singlesignon='singlesignon',
-              datasync='datasync',
-              macie='macie',
-              neptune='neptune',
-              timestream='timestream',
-              documentdb='documentdb',
-              msk='kafka',
-              amazonglacier='glacier',
-             )
+url_start = {
+    'https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/concepts.html': 'https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/Storage.html'
+}
 
-serviceD = {**{'opensearch-service':'opensearch',
-              'step-functions':'stepfunctions',
-              'lake-formation':'lakeformation',
-              'amazon-mq':'mq',
-              'lambda':'lambda'},**serviceD}
+serviceD = dict(streams='streams',
+                firehose='firehose',
+                kinesisanalytics='analytics',
+                athena='athena',
+                redshift='redshift',
+                quicksight='quicksight',
+                glue='glue',
+                emr='emr',
+                dms='dms',
+                kms='kms',
+                AmazonS3='s3',
+                datapipeline='datapipeline',
+                elasticache='elasticache',
+                directconnect='directconnect',
+                sagemaker='sagemaker',
+                sns='sns',
+                snowball='snowball',
+                amazondynamodb='dynamodb',
+                AWSSimpleQueueService='sqs',
+                AmazonElastiCache='elasticache',
+                singlesignon='singlesignon',
+                datasync='datasync',
+                macie='macie',
+                neptune='neptune',
+                timestream='timestream',
+                documentdb='documentdb',
+                msk='kafka',
+                amazonglacier='glacier',
+                AWSEC2='ebs',
+                AmazonRDS='rds',
+                secretsmanager='secretsmanager',
+                cloudhsm='cloudhsm',
+                IAM='iam',
+
+                )
+
+serviceD = {**{'opensearch-service': 'opensearch',
+               'step-functions': 'stepfunctions',
+               'lake-formation': 'lakeformation',
+               'amazon-mq': 'mq',
+               'lambda': 'lambda'}, **serviceD}
 
 guideD = dict(dg='developer',
-             user='user',
+              user='user',
               ug='user',
-             dev='developer',
-             mgmt='management',
-             gsg='getting-started',
-             ManagementGuide='management',
-             ReleaseGuide='release',
-             sbs='step-by-step',
-             developerguide='developer',
-             DeveloperGuide='developer',
-             SQSDeveloperGuide='developer',
+              dev='developer',
+              mgmt='management',
+              gsg='getting-started',
+              ManagementGuide='management',
+              ReleaseGuide='release',
+              sbs='step-by-step',
+              developerguide='developer',
+              DeveloperGuide='developer',
+              SQSDeveloperGuide='developer',
               UserGuide='user',
               userguide='user',
-              operatorguide='operator'
-             )
-guideD = {**{'red-ug':'redis','mem-ug':'memcache',
-            'developer-guide':'developer',
-            'migration-guide':'migration'}, **guideD}
+              operatorguide='operator',
+              AuroraUserGuide='aurora-user'
+              )
+guideD = {**{'red-ug': 'redis', 'mem-ug': 'memcache',
+             'developer-guide': 'developer',
+             'migration-guide': 'migration'}, **guideD}
